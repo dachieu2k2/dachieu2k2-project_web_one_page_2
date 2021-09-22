559fd4a48827559bbfa3c7329aaec0915e54d584
dachieu2k2 <hieutedam2k2@gmail.com>
2021-09-23 04:29:17 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,157 @@
+var turnOnLight = document.querySelector('.onlight');
+const body = document.querySelector('body');
+//home
+turnOnLight.onclick = function () {
+    // console.log(turnOnLight);
+
+    turnOnLight.classList.toggle('onlight-on');
+    turnOnLight.classList.toggle('onlight-off');
+
+    body.classList.toggle('on--light');
+}
+
+
+//skill
+const circleSkillNumber = document.querySelectorAll('.circle__skill-number');
+const attributeDataTargetSkill = [];
+const attributeSetTimeSkill = [];
+
+for(var i =0; i < circleSkillNumber.length;i++){
+    attributeDataTargetSkill[i] = circleSkillNumber[i].getAttribute('data-target');
+    attributeSetTimeSkill[i] = circleSkillNumber[i].getAttribute('set-time');
+    // console.log(attributeDataTargetSkill[i],attributeSetTimeSkill[i]);
+}
+
+
+for(var i =0; i < circleSkillNumber.length;i++){
+    animationPercent(circleSkillNumber[i],attributeDataTargetSkill[i],attributeSetTimeSkill[i]);
+}
+function animationPercent(element, value, time){
+    let dem = 0;
+    let interval = setInterval(function(){
+        if(dem == value){
+            clearInterval(interval);
+        }else {
+            dem ++;
+            return element.innerHTML = dem + '%';
+        }
+    }, time);
+}
+
+function animationBottomtoTop(something){
+    var selectorAllElements = document.querySelectorAll(something);
+    
+    // selectorAllElements.forEach(e =>{
+    //     e.style = `transform: translateY(200px);`;
+    //     e.style = `opacity: 0;`;
+    // } )
+    selectorAllElements.forEach((e,key) =>{
+        e.style = `animation: 1s bottom-to-top linear ${key}s forwards;`
+    } )
+}
+
+const skillItemPercent = document.querySelectorAll('.skills__item-percent');
+const skillsItemLineInline = document.querySelectorAll('.skills__item-line-inline');
+
+for(var i=0;i<skillItemPercent.length;i++){
+    skillsItemLineInline[i].style.width = 0;
+}
+// console.log(skillItemPercent);
+
+
+
+//mobile navbar 
+var buttonMenu = document.querySelector('.button-nav__nut');
+var buttonMenuClose = document.querySelector('.button-nav__nut-close');
+var navList = document.querySelector('.nav__list');
+
+buttonMenu.onclick = function(){
+    navList.classList.remove('hide-on-mobile');
+}
+buttonMenuClose.onclick = function(){
+    navList.classList.add('hide-on-mobile');
+}
+//scroll 
+const sections = document.querySelectorAll('section');
+const navItem = document.querySelectorAll('.nav__item');
+const header = document.querySelector('.header');
+const svg = document.querySelectorAll('svg');
+let count=0;
+console.log(body.classList.length);
+
+console.log();
+document.addEventListener('scroll', function(){
+if (body.classList.length === 0 || window.innerWidth < 1024){
+
+    let current = '';
+    const scroll_postition = window.scrollY;
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(scroll_postition >= (sectionTop - sectionHeight / 5)){
+            current = section.getAttribute('id');
+        }
+    
+    })
+    navItem.forEach( li =>{
+        li.classList.remove('active');
+        if(li.querySelector('a').getAttribute('href') == "#" + current){
+            li.classList.add('active');
+        }
+        
+        // console.log(key);
+    } )
+    
+    //navbar
+    if(scroll_postition >= (sections[1].offsetTop - sections[1].clientHeight / 5)){
+        header.style.backgroundColor = "var(--black-color)"
+    }else {
+        header.style.backgroundColor = "transparent";
+    }
+    //about
+    if(scroll_postition >= (sections[1].offsetTop - sections[1].clientHeight / 5)){
+        document.querySelector('.about__img').style = ` animation: bottom-to-top cubic-bezier(0.4, 0, 1, 1) 1s forwards;`;
+        document.querySelector('.about__content').style = ` animation: 1s bottom-to-top cubic-bezier(0.4, 0, 1, 1) 1s forwards;`;
+        
+    }
+    //education
+    if(scroll_postition >= (sections[2].offsetTop - sections[2].clientHeight / 5)){
+        var educationElements = document.querySelectorAll('.education__item');
+        educationElements.forEach((e,key) =>{
+        e.style = `animation: 1s bottom-to-top linear ${key}s forwards;`
+        } )
+    }
+
+
+    //skills
+    if(scroll_postition >= (sections[3].offsetTop - sections[3].clientHeight / 5)){
+        for(var i=0;i<svg.length;i++){
+            if(count<4){
+            svg[i].querySelector('circle').classList.add(`circle-${i+1}`);
+            animationPercent(circleSkillNumber[i],attributeDataTargetSkill[i],attributeSetTimeSkill[i]);
+            }
+            count++;
+        }
+        const css = window.document.styleSheets[4];
+for(var i=0;i<skillItemPercent.length;i++){
+    if(count<5){
+        animationPercent(skillItemPercent[i],skillItemPercent[i].getAttribute('data-target'),skillItemPercent[i].getAttribute('set-time'));
+    css.insertRule(`
+@keyframes right-to-left${i} {
+  100% { width: ${skillItemPercent[i].innerHTML}; }
+}`);
+    skillsItemLineInline[i].style.animation = ` right-to-left${i} 2s forwards`;
+}
+        
+    }
+    }
+    
+    // console.log(current,current2);
+    //contact
+    if(scroll_postition >= (sections[4].offsetTop - sections[4].clientHeight / 5)){}
+    
+}
+
+})
+    
+// console.log(svg);
